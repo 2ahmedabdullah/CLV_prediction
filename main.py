6eefec637364bccd5428fc01dfb9f88086512085
Abdul Ahmed <2ahmedabdullah@gmail.com>
2022-01-15 01:08:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ from datetime import datetime, timedelta
 from scipy.stats import pearsonr, chi2_contingency
 from utils import *
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
+from xgboost import XGBRegressor, XGBClassifier
 from sklearn.model_selection import RepeatedKFold, cross_val_score
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import classification_report
 
 
 data_path = './data/'
@@ -50,6 +53,7 @@ if __name__ == '__main__':
     new_data = pd.merge(x, y, on='Id')
     new_data.to_pickle('ready_data.pkl')
     
+#    new_data = pd.read_pickle('ready_data.pkl')
     
     X = new_data[['Recency', 'Qty', 'Price_x', 'Mean']]
     y_prob = new_data['y']
@@ -68,9 +72,14 @@ if __name__ == '__main__':
     
     yhat = model.predict(X)
 
+    plt.figure(figsize = (12,9))
     plt.scatter(y_spend, yhat)
     p1, p2 = [0, max(y_spend)], [0, max(y_spend)]
-    plt.plot(p1, p2, color ='red') 
+    plt.plot(p1, p2, color ='red')
+    plt.title('REGRESSOR: Spend Prediction')
+    plt.xlabel('y_actual')
+    plt.ylabel('y_pred')
+    plt.savefig(plot_path+'price_prediction.png')
     plt.show()
     
     corr, _ = pearsonr(y_spend, yhat)
@@ -92,6 +101,22 @@ if __name__ == '__main__':
     y_proba = yproba[:, 1]
     
     print(classification_report(y_prob, yhat1))
+    auc_score = roc_auc_score(y_prob, yhat1)
+    auc_score = round(auc_score, 2)
+    print ('AUC SCORE: ', auc_score)
+
+    fpr, tpr, thresh = metrics.roc_curve(yhat1, y_prob)
+    plt.figure(figsize = (12,9))
+    plt.plot(fpr, tpr, label="XGB, AUC="+str(auc_score))
+    plt.xlabel('FPR', fontsize=10)
+    plt.ylabel('TPR', fontsize=10)
+    p1, p2 = [0,1], [0, 1]
+    plt.plot(p1, p2, color ='red', label = 'Baseline, AUC=0.50')
+    plt.legend(loc=4)    
+    plt.title('CLASSIFIER: ROC Curve')
+    plt.savefig(plot_path+'ROC.png')
+    plt.show()
+
 
     dash_app = new_data[['Id', 'Qty', 'Price_y', 'y']]
     dash_app['Pred_Price_y'] = list(yhat)
@@ -99,8 +124,22 @@ if __name__ == '__main__':
     
     dash_app.to_pickle('dash_app.pkl')
 
-    plt.scatter(dash_app['Price_y'], dash_app['Pred_y'])
+
+
+    plt.figure(figsize = (12,9))
+    num_colors = 3
+    cmap = plt.get_cmap('viridis', num_colors)
+    cmap.set_under('red')
+    plt.scatter(dash_app['Price_y'], dash_app['Pred_y'], c=dash_app['Pred_y'], cmap=cmap, vmin=0, vmax=1)
+    plt.colorbar(extend='min')
+    plt.title('Purchase Value vs Purchase Probability')
+    plt.xlabel('Purchase Value', fontsize=10)
+    plt.ylabel('Probability of Purchase', fontsize=10)
+    plt.savefig(plot_path+'plot1.png')
     plt.show()
+
+
+
 
 
 
